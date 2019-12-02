70fffaa22b0756eef665dea4e3e01672bd36c8c4
Ray He <rayhe921@gmail.com>
2019-12-01 23:12:30 -0800

    inital files

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE burger_db;
+
+INSERT INTO burgers ("burger_name")
+VALUES ("turkey burger");
+
+INSERT INTO burgers ("burger_name")
+VALUES ("teriyaki burger");
+
+INSERT INTO burgers ("burger_name")
+VALUES ("impossible burger");
